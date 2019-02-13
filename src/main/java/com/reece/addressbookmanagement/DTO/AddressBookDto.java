18d33209022a799c2dfaccfbe9892ed2e91e3504
John Adolfo <>
2019-02-13 16:29:48 +1100

    AddressBookDTo

---
+++ src/main/java/com/reece/addressbookmanagement/DTO/AddressBookDto.java
@@ -0,0 +1,17 @@
+package com.reece.addressbookmanagement.DTO;
+
+import com.reece.addressbookmanagement.model.Contact;
+
+import java.util.Collection;
+
+public class AddressBookDto {
+
+    private Long id;
+
+    private Collection<Contact> contacts;
+
+    public Collection<Contact> getContacts() { return contacts; }
+
+    public void setContacts(Collection<Contact> contacts){this.contacts = contacts;}
+
+}
